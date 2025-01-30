de14fdc6612f22a6f7865cdf2c080f05db0320b0
erkanbeskardes <erkanbeskardes@outlook.com>
2025-01-30 11:34:06 +0300

    Project created.

---
+++ src/main/java/com/erkanbeskardes/isbul/IsbulApplication.java
@@ -0,0 +1,13 @@
+package com.erkanbeskardes.isbul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsbulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erkanbeskardes/isbul/IsbulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erkanbeskardes.isbul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
